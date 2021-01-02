9c0f8b1ff864b4fd4318102427f88c517b13237c
NaturaAurum <taewoo.kim@5minlab.com>
2021-01-02 18:51:51 +0900

    적, 소행성 Generate Script 제작

---
+++ Assets/Scripts/AsteroidGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidGenerator : MonoBehaviour
+{
+    public GameObject[] AsteroidPrefabs = null;
+    public Transform[] Points = null;
+
+    public float GenerateTime = 1f;
+    private float timer = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= GenerateTime)
+        {
+            GenerateAsteroid();
+            timer = 0;
+        }
+    }
+
+    private void GenerateAsteroid()
+    {
+        int objectRandomIndex = Random.Range(0, AsteroidPrefabs.Length);
+        GameObject targetPrefab = AsteroidPrefabs[objectRandomIndex];
+        int pointRandomIndex = Random.Range(0, Points.Length);
+        Transform targetPoint = Points[pointRandomIndex];
+        Instantiate(targetPrefab, targetPoint.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject EnemyPrefab = null;
+    public Transform[] Points = null;
+
+    public float GenerateTime = 1f;
+    private float timer = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= GenerateTime)
+        {
+            GenerateEnemy();
+            timer = 0f;
+        }
+    }
+
+    private void GenerateEnemy()
+    {
+        int randomIndex = Random.Range(0, Points.Length);
+        Transform randomPoint = Points[randomIndex];
+        Instantiate(EnemyPrefab, randomPoint.position, Quaternion.identity);
+    }
+}
